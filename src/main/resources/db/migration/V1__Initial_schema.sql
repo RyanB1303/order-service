7618b123c0a6421c31d9028320e17cf1a5f07392
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-04-08 19:05:09 +0700

    fix flyway config

--- src/main/resources/db/migration/V1__Initial_schema.sql
+++ src/main/resources/db/migration/V1__Initial_schema.sql
@@ -7,5 +7,5 @@ CREATE TABLE orders (
     status              VARCHAR(255) NOT NULL,
     created_date        timestamp NOT NULL,
     last_modified_date  timestamp NOT NULL,
-    version             integer NOT NULL,
-)
+    version             integer NOT NULL
+);
