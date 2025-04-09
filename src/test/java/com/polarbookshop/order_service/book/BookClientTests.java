1b6e1c1e2b2013ca6ed9d412f04e44b922456cb8
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-04-09 09:24:19 +0700

    Testing Reactive application - rest controller with WebFlux (broken)

--- src/test/java/com/polarbookshop/order_service/book/BookClientTests.java
+++ src/test/java/com/polarbookshop/order_service/book/BookClientTests.java
@@ -61,4 +61,19 @@ class BookClientTests {
                 )
                 .verifyComplete();
     }
+
+    @Test
+    void whenBookNotExistsThenReturnEmpty() {
+        var bookIsbn = "1234567895";
+
+        var mockResponse = new MockResponse()
+                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .setResponseCode(404);
+
+        mockWebServer.enqueue(mockResponse);
+
+        StepVerifier.create(bookClient.getBookByIsbn(bookIsbn))
+                .expectNextCount(0)
+                .verifyComplete();
+    }
 }
